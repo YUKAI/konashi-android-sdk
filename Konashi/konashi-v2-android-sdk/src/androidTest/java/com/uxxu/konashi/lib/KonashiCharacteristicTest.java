c14ce6c347513d5994860edbcf381870ab4534f9
izumin5210 <masayuki@izumin.info>
2015-08-09 10:45:26 +0900

    Group test cases

--- Konashi/konashi-v2-android-sdk/src/androidTest/java/com/uxxu/konashi/lib/KonashiCharacteristicTest.java
+++ Konashi/konashi-v2-android-sdk/src/androidTest/java/com/uxxu/konashi/lib/KonashiCharacteristicTest.java
@@ -7,6 +7,7 @@ import com.uxxu.konashi.lib.listeners.KonashiListener;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -18,95 +19,124 @@ import java.util.UUID;
 /**
  * Created by izumin on 8/4/15.
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(Enclosed.class)
 public class KonashiCharacteristicTest {
     public static final String TAG = KonashiCharacteristicTest.class.getSimpleName();
 
-    @Spy private KonashiNotifier mNotifier;
-    @Mock private KonashiListener mListener;
-    @Mock private BluetoothGattCharacteristic mCharacteristic;
+    private static class BaseTest {
+        @Spy private KonashiNotifier mNotifier;
+        @Mock private KonashiListener mListener;
+        @Mock private BluetoothGattCharacteristic mCharacteristic;
 
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        mNotifier.addListener(mListener);
+        @Before
+        public void setUp() {
+            MockitoAnnotations.initMocks(this);
+            mNotifier.addListener(mListener);
+        }
+
+        public KonashiNotifier getNotifier() {
+            return mNotifier;
+        }
+
+        public KonashiListener getListener() {
+            return mListener;
+        }
+
+        public BluetoothGattCharacteristic getCharacteristic() {
+            return mCharacteristic;
+        }
     }
 
-    @Test
-    public void testHandleAnalogRead0() {
-        Mockito.when(mCharacteristic.getValue()).thenReturn(new byte[] {0x03, 0x48});
-        KonashiCharacteristic
-                .valueOf(KonashiUUID.ANALOG_READ0_UUID)
-                .handle(mCharacteristic, mNotifier);
-        Mockito.verify(mListener, Mockito.times(1)).onUpdateAnalogValueAio0(840);
+    @RunWith(AndroidJUnit4.class)
+    public static class AnalogCharacteristicTest extends BaseTest {
+        @Test
+        public void testHandleAnalogRead0() {
+            Mockito.when(getCharacteristic().getValue()).thenReturn(new byte[]{0x03, 0x48});
+            KonashiCharacteristic
+                    .valueOf(KonashiUUID.ANALOG_READ0_UUID)
+                    .handle(getCharacteristic(), getNotifier());
+            Mockito.verify(getListener(), Mockito.times(1)).onUpdateAnalogValueAio0(840);
+        }
+
+        @Test
+        public void testHandleAnalogRead1() {
+            Mockito.when(getCharacteristic().getValue()).thenReturn(new byte[]{0x03, 0x48});
+            KonashiCharacteristic
+                    .valueOf(KonashiUUID.ANALOG_READ1_UUID)
+                    .handle(getCharacteristic(), getNotifier());
+            Mockito.verify(getListener(), Mockito.times(1)).onUpdateAnalogValueAio1(840);
+        }
+
+        @Test
+        public void testHandleAnalogRead2() {
+            Mockito.when(getCharacteristic().getValue()).thenReturn(new byte[]{0x03, 0x48});
+            KonashiCharacteristic
+                    .valueOf(KonashiUUID.ANALOG_READ2_UUID)
+                    .handle(getCharacteristic(), getNotifier());
+            Mockito.verify(getListener(), Mockito.times(1)).onUpdateAnalogValueAio2(840);
+        }
     }
 
-    @Test
-    public void testHandleAnalogRead1() {
-        Mockito.when(mCharacteristic.getValue()).thenReturn(new byte[] {0x03, 0x48});
-        KonashiCharacteristic
-                .valueOf(KonashiUUID.ANALOG_READ1_UUID)
-                .handle(mCharacteristic, mNotifier);
-        Mockito.verify(mListener, Mockito.times(1)).onUpdateAnalogValueAio1(840);
+    @RunWith(AndroidJUnit4.class)
+    public static class PioCharacteristicTest extends BaseTest {
+        @Test
+        public void testHandlePioSetting() {
+            Mockito.when(getCharacteristic().getValue()).thenReturn(new byte[]{0x08});
+            KonashiCharacteristic
+                    .valueOf(KonashiUUID.PIO_SETTING_UUID)
+                    .handle(getCharacteristic(), getNotifier());
+            Mockito.verify(getListener(), Mockito.times(1)).onUpdatePioSetting((byte) 0x08);
+        }
+
+        @Test
+        public void testHandlePioPullup() {
+            Mockito.when(getCharacteristic().getValue()).thenReturn(new byte[]{0x08});
+            KonashiCharacteristic
+                    .valueOf(KonashiUUID.PIO_PULLUP_UUID)
+                    .handle(getCharacteristic(), getNotifier());
+            Mockito.verify(getListener(), Mockito.times(1)).onUpdatePioPullup((byte) 0x08);
+        }
+
+        @Test
+        public void testHandlePioNotification() {
+            Mockito.when(getCharacteristic().getValue()).thenReturn(new byte[]{0x13});
+            KonashiCharacteristic
+                    .valueOf(KonashiUUID.PIO_INPUT_NOTIFICATION_UUID)
+                    .handle(getCharacteristic(), getNotifier());
+            Mockito.verify(getListener(), Mockito.times(1)).onUpdatePioInput((byte) 0x13);
+        }
     }
 
-    @Test
-    public void testHandleAnalogRead2() {
-        Mockito.when(mCharacteristic.getValue()).thenReturn(new byte[] {0x03, 0x48});
-        KonashiCharacteristic
-                .valueOf(KonashiUUID.ANALOG_READ2_UUID)
-                .handle(mCharacteristic, mNotifier);
-        Mockito.verify(mListener, Mockito.times(1)).onUpdateAnalogValueAio2(840);
+    @RunWith(AndroidJUnit4.class)
+    public static class UartCharacteristicTest extends BaseTest {
+        @Test
+        public void testHandleUartRxNotification() {
+            Mockito.when(getCharacteristic().getValue()).thenReturn("test".getBytes());
+            KonashiCharacteristic
+                    .valueOf(KonashiUUID.UART_RX_NOTIFICATION_UUID)
+                    .handle(getCharacteristic(), getNotifier());
+            Mockito.verify(getListener(), Mockito.times(1)).onCompleteUartRx(new byte[]{0x74, 0x65, 0x73, 0x74});
+        }
     }
 
-    @Test
-    public void testHandleBatteryLevel() {
-        Mockito.when(mCharacteristic.getValue()).thenReturn(new byte[] {0x48});
-        KonashiCharacteristic
-                .valueOf(KonashiUUID.BATTERY_LEVEL_UUID)
-                .handle(mCharacteristic, mNotifier);
-        Mockito.verify(mListener, Mockito.times(1)).onUpdateBatteryLevel(72);
+    @RunWith(AndroidJUnit4.class)
+    public static class DeviceInfoCharacteristicTest extends BaseTest {
+        @Test
+        public void testHandleBatteryLevel() {
+            Mockito.when(getCharacteristic().getValue()).thenReturn(new byte[]{0x48});
+            KonashiCharacteristic
+                    .valueOf(KonashiUUID.BATTERY_LEVEL_UUID)
+                    .handle(getCharacteristic(), getNotifier());
+            Mockito.verify(getListener(), Mockito.times(1)).onUpdateBatteryLevel(72);
+        }
     }
 
-    @Test
-    public void testHandlePioSetting() {
-        Mockito.when(mCharacteristic.getValue()).thenReturn(new byte[] {0x08});
-        KonashiCharacteristic
-                .valueOf(KonashiUUID.PIO_SETTING_UUID)
-                .handle(mCharacteristic, mNotifier);
-        Mockito.verify(mListener, Mockito.times(1)).onUpdatePioSetting((byte) 0x08);
-    }
-
-    @Test
-    public void testHandlePioPullup() {
-        Mockito.when(mCharacteristic.getValue()).thenReturn(new byte[] {0x08});
-        KonashiCharacteristic
-                .valueOf(KonashiUUID.PIO_PULLUP_UUID)
-                .handle(mCharacteristic, mNotifier);
-        Mockito.verify(mListener, Mockito.times(1)).onUpdatePioPullup((byte) 0x08);
-    }
-
-    @Test
-    public void testHandlePioNotification() {
-        Mockito.when(mCharacteristic.getValue()).thenReturn(new byte[] {0x13});
-        KonashiCharacteristic
-                .valueOf(KonashiUUID.PIO_INPUT_NOTIFICATION_UUID)
-                .handle(mCharacteristic, mNotifier);
-        Mockito.verify(mListener, Mockito.times(1)).onUpdatePioInput((byte) 0x13);
-    }
-
-    @Test
-    public void testHandleUartRxNotification() {
-        Mockito.when(mCharacteristic.getValue()).thenReturn("test".getBytes());
-        KonashiCharacteristic
-                .valueOf(KonashiUUID.UART_RX_NOTIFICATION_UUID)
-                .handle(mCharacteristic, mNotifier);
-        Mockito.verify(mListener, Mockito.times(1)).onCompleteUartRx(new byte[] {0x74, 0x65, 0x73, 0x74});
-    }
-
-    @Test
-    public void testHandleUnknownCharacteristic() {
-        KonashiCharacteristic.valueOf(UUID.randomUUID()).handle(mCharacteristic, mNotifier);
-        Mockito.verifyNoMoreInteractions(mListener);
+    @RunWith(AndroidJUnit4.class)
+    public static class UnknownCharacteristicTest extends BaseTest {
+        @Test
+        public void testHandleUnknownCharacteristic() {
+            KonashiCharacteristic.valueOf(UUID.randomUUID()).handle(getCharacteristic(), getNotifier());
+            Mockito.verifyNoMoreInteractions(getListener());
+        }
     }
 }
