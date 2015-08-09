a33605757f785b72d04d44fcf5c560fea6a00967
izumin5210 <masayuki@izumin.info>
2015-08-09 12:32:37 +0900

    Add KonashiPwmEvent

---
+++ Konashi/konashi-v2-android-sdk/src/main/java/com/uxxu/konashi/lib/events/KonashiPwmEvent.java
@@ -0,0 +1,35 @@
+package com.uxxu.konashi.lib.events;
+
+import com.uxxu.konashi.lib.listeners.KonashiBaseListener;
+import com.uxxu.konashi.lib.listeners.KonashiPwmListener;
+
+/**
+ * Created by izumin on 8/9/15.
+ */
+public enum KonashiPwmEvent implements KonashiEvent {
+    UPDATE_PWM_CONFIG {
+        @Override
+        protected void notifyPwmEvent(Object param0, Object param1, KonashiPwmListener listener) {
+            listener.onUpdatePwmMode(Integer.valueOf(param0.toString()));
+        }
+    },
+    UPDATE_PWM_PARAM {
+        @Override
+        protected void notifyPwmEvent(Object param0, Object param1, KonashiPwmListener listener) {
+            listener.onUpdatePwmPeriod(Integer.valueOf(param0.toString()), Integer.valueOf(param1.toString()));
+        }
+    },
+    UPDATE_PWM_DUTY {
+        @Override
+        protected void notifyPwmEvent(Object param0, Object param1, KonashiPwmListener listener) {
+            listener.onUpdatePwmDuty(Integer.valueOf(param0.toString()), Integer.valueOf(param1.toString()));
+        }
+    };
+
+    abstract protected void notifyPwmEvent(Object param0, Object param1, KonashiPwmListener listener);
+
+    @Override
+    public void notify(Object param0, Object param1, KonashiBaseListener listener) {
+        notifyPwmEvent(param0, param1, (KonashiPwmListener) listener);
+    }
+}
