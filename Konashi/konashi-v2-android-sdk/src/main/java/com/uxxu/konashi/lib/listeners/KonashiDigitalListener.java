81ee9b824abc29bef4a3cc34404f21f0690357e2
izumin5210 <masayuki@izumin.info>
2015-08-09 10:17:34 +0900

    Add 2 digital events and their callback methods

--- Konashi/konashi-v2-android-sdk/src/main/java/com/uxxu/konashi/lib/listeners/KonashiDigitalListener.java
+++ Konashi/konashi-v2-android-sdk/src/main/java/com/uxxu/konashi/lib/listeners/KonashiDigitalListener.java
@@ -5,6 +5,14 @@ package com.uxxu.konashi.lib.listeners;
  */
 public interface KonashiDigitalListener extends KonashiBaseListener {
     /**
+     * PIOのピンの設定が変化した時に呼ばれる
+     */
+    void onUpdatePioSetting(int modes);
+    /**
+     * PIOのピンをプルアップするかの設定が変化した時に呼ばれる
+     */
+    void onUpdatePioPullup(int pullups);
+    /**
      * PIOの入力の状態が変化した時に呼ばれる
      */
     void onUpdatePioInput(byte value);
