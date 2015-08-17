ed5397f181354939b270c4c821155f5d62c0e6ac
e10dokup <1365196t@stu.kobe-u.ac.jp>
2015-08-17 23:46:30 +0900

    Generate MainActivityFragment

---
+++ samples/pwm-sample/src/main/java/com/uxxu/konashi/sample/pwmsample/MainActivityFragment.java
@@ -0,0 +1,9 @@
+package com.uxxu.konashi.sample.pwmsample;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+}
