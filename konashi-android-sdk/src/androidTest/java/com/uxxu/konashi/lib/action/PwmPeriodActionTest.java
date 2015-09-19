47a2152695cb242e48cc80962aeeaeb58f3d3383
izumin5210 <masayuki@izumin.info>
2015-09-19 11:38:41 +0900

    Add tests for PWM actions

---
+++ konashi-android-sdk/src/androidTest/java/com/uxxu/konashi/lib/action/PwmPeriodActionTest.java
@@ -0,0 +1,60 @@
+package com.uxxu.konashi.lib.action;
+
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by izumin on 9/18/15.
+ */
+public class PwmPeriodActionTest {
+
+    @Mock private BluetoothGattService mService;
+    @Mock private BluetoothGattCharacteristic mCharacteristic;
+
+    private PwmPeriodAction mAction;
+
+    private ArgumentCaptor<byte[]> mValueCaptor;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mValueCaptor = ArgumentCaptor.forClass(byte[].class);
+        when(mService.getCharacteristic(any(UUID.class))).thenReturn(mCharacteristic);
+    }
+
+    @Test
+    public void hasValidParams_WithInvalidModes() throws Exception {
+        mAction = new PwmPeriodAction(mService, 2, 3, 4);
+        assertThat(mAction.hasValidParams()).isFalse();
+    }
+
+    @Test
+    public void hasValidParams_WithValidModes() throws Exception {
+        mAction = new PwmPeriodAction(mService, 2, 4, 3);
+        assertThat(mAction.hasValidParams()).isTrue();
+    }
+
+    @Test
+    public void setValue_WithValidParams() throws Exception {
+        mAction = new PwmPeriodAction(mService, 2, 5000000, 3);
+        mAction.setValue();
+        verify(mCharacteristic, times(1)).setValue(mValueCaptor.capture());
+        assertThat(mValueCaptor.getValue()).isEqualTo(
+                new byte[]{(byte) 0x02, (byte) 0x00, (byte) 0x4c, (byte) 0x4b, (byte) 0x40}
+        );
+    }
+}
