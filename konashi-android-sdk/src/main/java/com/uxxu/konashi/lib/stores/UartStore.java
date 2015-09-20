35b5d0a906a3487e45b7b071d0a62bfdda1ba79c
e10dokup <1365196t@stu.kobe-u.ac.jp>
2015-09-20 12:32:57 +0900

    Add UartStore

---
+++ konashi-android-sdk/src/main/java/com/uxxu/konashi/lib/stores/UartStore.java
@@ -0,0 +1,33 @@
+package com.uxxu.konashi.lib.stores;
+
+import com.uxxu.konashi.lib.dispatcher.CharacteristicDispatcher;
+import com.uxxu.konashi.lib.dispatcher.UartStoreUpdater;
+
+/**
+ * Created by e10dokup on 9/20/15.
+ */
+public class UartStore implements Store {
+
+    private byte mUartSetting;
+    private byte mUartBaudrate;
+
+    public UartStore(CharacteristicDispatcher<UartStore, UartStoreUpdater> dispatcher) {
+        dispatcher.getStore(this);
+    }
+
+    public byte getUartSetting() {
+        return mUartSetting;
+    }
+
+    public void setUartSetting(byte uartSetting) {
+        mUartSetting = uartSetting;
+    }
+
+    public byte getUartBaudrate() {
+        return mUartBaudrate;
+    }
+
+    public void setUartBaudrate(byte uartBaudrate) {
+        mUartBaudrate = uartBaudrate;
+    }
+}
