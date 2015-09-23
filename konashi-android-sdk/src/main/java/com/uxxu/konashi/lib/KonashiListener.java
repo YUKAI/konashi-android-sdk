654efcfc4147a72accecab81ce4b0e454b04ac05
izumin5210 <masayuki@izumin.info>
2015-09-23 21:38:34 +0900

    Add KonashiListener

---
+++ konashi-android-sdk/src/main/java/com/uxxu/konashi/lib/KonashiListener.java
@@ -0,0 +1,15 @@
+package com.uxxu.konashi.lib;
+
+import info.izumin.android.bletia.BletiaException;
+
+/**
+ * Created by izumin on 9/23/15.
+ */
+public interface KonashiListener {
+    void onConnect(KonashiManager manager);
+    void onDisconnect(KonashiManager manager);
+    void onError(KonashiManager manager, BletiaException e);
+    void onUpdatePioOutput(KonashiManager manager, int value);
+    void onUpdateUartRx(KonashiManager manager, byte[] value);
+    void onUpdateBatteryLevel(KonashiManager manager, int level);
+}
