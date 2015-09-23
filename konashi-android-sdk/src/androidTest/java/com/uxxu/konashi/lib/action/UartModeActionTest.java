bc3a5bf9747991ae550f2370dd7fcc5c9dd3caa1
e10dokup <1365196t@stu.kobe-u.ac.jp>
2015-09-23 21:47:25 +0900

    Add UartModeActionTest

---
+++ konashi-android-sdk/src/androidTest/java/com/uxxu/konashi/lib/action/UartModeActionTest.java
@@ -0,0 +1,63 @@
+package com.uxxu.konashi.lib.action;
+
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+
+import com.uxxu.konashi.lib.Konashi;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by e10dokup on 2015/09/23
+ **/
+public class UartModeActionTest {
+    private static final String TAG = UartModeActionTest.class.getSimpleName();
+    private final UartModeActionTest self = this;
+
+    @Mock
+    private BluetoothGattService mService;
+    @Mock private BluetoothGattCharacteristic mCharacteristic;
+
+    private UartModeAction mAction;
+
+    private ArgumentCaptor<byte[]> mValueCaptor;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mValueCaptor = ArgumentCaptor.forClass(byte[].class);
+        when(mService.getCharacteristic(any(UUID.class))).thenReturn(mCharacteristic);
+    }
+
+    @Test
+    public void hasValidParams_ForValidValue() throws Exception {
+        mAction = new UartModeAction(mService, Konashi.UART_ENABLE);
+        assertThat(mAction.hasValidParams()).isTrue();
+    }
+
+    @Test
+    public void hasValidParams_FotInvalidValue() throws Exception {
+        mAction = new UartModeAction(mService, 100);
+        assertThat(mAction.hasValidParams()).isFalse();
+    }
+
+    @Test
+    public void setValue() throws Exception {
+        mAction = new UartModeAction(mService, Konashi.UART_ENABLE);
+        mAction.setValue();
+        verify(mCharacteristic, times(1)).setValue(mValueCaptor.capture());
+        assertThat(mValueCaptor.getValue()[0]).isEqualTo((byte) Konashi.UART_ENABLE);
+    }
+}
