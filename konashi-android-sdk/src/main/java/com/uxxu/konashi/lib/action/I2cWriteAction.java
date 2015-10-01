a80de83a8bf57de38b3150b7418f27fcfbe6f042
e10dokup <1365196t@stu.kobe-u.ac.jp>
2015-10-02 00:08:31 +0900

    Fix run constructor twice

--- konashi-android-sdk/src/main/java/com/uxxu/konashi/lib/action/I2cWriteAction.java
+++ konashi-android-sdk/src/main/java/com/uxxu/konashi/lib/action/I2cWriteAction.java
@@ -2,6 +2,7 @@ package com.uxxu.konashi.lib.action;
 
 import android.bluetooth.BluetoothGattService;
 
+import com.uxxu.konashi.lib.Konashi;
 import com.uxxu.konashi.lib.KonashiUUID;
 import com.uxxu.konashi.lib.store.I2cStore;
 import com.uxxu.konashi.lib.util.I2cUtils;
@@ -14,6 +15,8 @@ import java.util.UUID;
 public class I2cWriteAction extends I2cAction {
 
     private static final UUID UUID = KonashiUUID.I2C_WRITE_UUID;
+    private static final int MAX_VALUE_SIZE = 20;
+
 
     private final byte mAddress;
     private final byte[] mData;
@@ -26,8 +29,8 @@ public class I2cWriteAction extends I2cAction {
 
     @Override
     protected void setValue() {
-        byte[] value = new byte[20];
-        if(mData.length < 20) value = new byte[mData.length + 2];
+        byte[] value;
+        value = ((mData.length < Konashi.I2C_DATA_MAX_LENGTH) ? new byte[mData.length + 2] : new byte[MAX_VALUE_SIZE]);
         value[0] = (byte) (mData.length + 1);
         value[1] = (byte) ((mAddress << 1) & 0xfe);
         System.arraycopy(mData, 0, value, 2, mData.length);
