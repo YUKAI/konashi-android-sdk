e54a2cbbb238412deb048f6eebef8611b0ff5507
e10dokup <1365196t@stu.kobe-u.ac.jp>
2015-10-02 01:23:29 +0900

    Add test of border length

--- konashi-android-sdk/src/androidTest/java/com/uxxu/konashi/lib/action/I2cWriteActionTest.java
+++ konashi-android-sdk/src/androidTest/java/com/uxxu/konashi/lib/action/I2cWriteActionTest.java
@@ -3,6 +3,7 @@ package com.uxxu.konashi.lib.action;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
 
+import com.uxxu.konashi.lib.Konashi;
 import com.uxxu.konashi.lib.store.I2cStore;
 
 import org.junit.Before;
@@ -61,10 +62,62 @@ public class I2cWriteActionTest {
         mAction = new I2cWriteAction(mService, (byte) 0x53 , mData, mStore);
         mAction.setValue();
         verify(mCharacteristic, times(1)).setValue(mValueCaptor.capture());
-        assertThat(mValueCaptor.getValue()[0]).isEqualTo((byte) 0x0a);
+        assertThat(mValueCaptor.getValue()[0]).isEqualTo((byte) (0x0a + 1));
         assertThat(mValueCaptor.getValue()[1]).isEqualTo((byte) 0xa6);
         for (int i = 0; i < mData.length; i++) {
             assertThat(mValueCaptor.getValue()[i+2]).isEqualTo(mData[i]);
+        }
+    }
+
+    @Test
+    public void setValue_WithDataSize0Params() throws Exception {
+        mAction = new I2cWriteAction(mService, (byte) 0x53, new byte[0], mStore);
+        mAction.setValue();
+        verify(mCharacteristic, times(1)).setValue(mValueCaptor.capture());
+        assertThat(mValueCaptor.getValue()[0]).isEqualTo((byte) (0x00 + 1));
+        assertThat(mValueCaptor.getValue()[1]).isEqualTo((byte) 0xa6);
+    }
+
+    @Test
+    public void setValue_WithDataSize1Params() throws Exception {
+        byte[] data = new byte[]{0x01};
+        mAction = new I2cWriteAction(mService, (byte) 0x53, data, mStore);
+        mAction.setValue();
+        verify(mCharacteristic, times(1)).setValue(mValueCaptor.capture());
+        assertThat(mValueCaptor.getValue()[0]).isEqualTo((byte) (0x01 + 1));
+        assertThat(mValueCaptor.getValue()[1]).isEqualTo((byte) 0xa6);
+        for (int i = 0; i < data.length; i++) {
+            assertThat(mValueCaptor.getValue()[i+2]).isEqualTo(data[i]);
+        }
+    }
+
+    @Test
+    public void setValue_WithDataSize16Params() throws Exception {
+        byte[] data = new byte[]{
+                0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
+                0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+        mAction = new I2cWriteAction(mService, (byte) 0x53, data, mStore);
+        mAction.setValue();
+        verify(mCharacteristic, times(1)).setValue(mValueCaptor.capture());
+        assertThat(mValueCaptor.getValue()[0]).isEqualTo((byte) (0x10 + 1));
+        assertThat(mValueCaptor.getValue()[1]).isEqualTo((byte) 0xa6);
+        for (int i = 0; i < data.length; i++) {
+            assertThat(mValueCaptor.getValue()[i+2]).isEqualTo(data[i]);
+        }
+    }
+
+    @Test
+    public void setValue_WithDataSize17Params() throws Exception {
+        byte[] data = new byte[]{
+                0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
+                0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10};
+        mAction = new I2cWriteAction(mService, (byte) 0x53, data, mStore);
+        mAction.setValue();
+        verify(mCharacteristic, times(1)).setValue(mValueCaptor.capture());
+        assertThat(mValueCaptor.getValue()[0]).isEqualTo((byte) (0x10 + 1));
+        assertThat(mValueCaptor.getValue()[1]).isEqualTo((byte) 0xa6);
+        for (int i = 0; i < Konashi.I2C_DATA_MAX_LENGTH; i++) {
+            assertThat(mValueCaptor.getValue()[i+2]).isEqualTo(data[i]);
         }
     }
 }
