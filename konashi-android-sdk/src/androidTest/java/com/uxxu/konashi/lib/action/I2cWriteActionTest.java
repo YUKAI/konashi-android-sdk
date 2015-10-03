e46a20e43add35dba9144bce9d42328eab89d3c3
e10dokup <1365196t@stu.kobe-u.ac.jp>
2015-10-03 10:25:22 +0900

    Modify I2cWriteActionTest TestCase

--- konashi-android-sdk/src/androidTest/java/com/uxxu/konashi/lib/action/I2cWriteActionTest.java
+++ konashi-android-sdk/src/androidTest/java/com/uxxu/konashi/lib/action/I2cWriteActionTest.java
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
 
 import com.uxxu.konashi.lib.Konashi;
+import com.uxxu.konashi.lib.KonashiErrorType;
 import com.uxxu.konashi.lib.store.I2cStore;
 
 import org.junit.Before;
@@ -48,13 +49,13 @@ public class I2cWriteActionTest {
                 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
                 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14
         }, mStore);
-        assertThat(mAction.hasValidParams()).isFalse();
+        assertThat(mAction.validate()).isEqualTo(KonashiErrorType.DATA_SIZE_TOO_LONG);
     }
 
     @Test
     public void hasValidParams_WithValidParams() throws Exception {
         mAction = new I2cWriteAction(mService, (byte) 0xa6, mData, mStore);
-        assertThat(mAction.hasValidParams()).isTrue();
+        assertThat(mAction.validate()).isEqualTo(KonashiErrorType.NO_ERROR);
     }
 
     @Test
