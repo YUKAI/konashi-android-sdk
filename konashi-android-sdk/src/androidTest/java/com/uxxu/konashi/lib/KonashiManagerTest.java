ab3cdff27990445918aac6959987c809692d56b0
izumin5210 <masayuki@izumin.info>
2015-10-05 20:36:33 +0900

    Fix broken test

--- konashi-android-sdk/src/androidTest/java/com/uxxu/konashi/lib/KonashiManagerTest.java
+++ konashi-android-sdk/src/androidTest/java/com/uxxu/konashi/lib/KonashiManagerTest.java
@@ -9,7 +9,6 @@ import com.uxxu.konashi.lib.action.AioAnalogReadAction;
 import com.uxxu.konashi.lib.action.BatteryLevelReadAction;
 import com.uxxu.konashi.lib.action.HardwareResetAction;
 import com.uxxu.konashi.lib.action.I2cModeAction;
-import com.uxxu.konashi.lib.action.I2cReadAction;
 import com.uxxu.konashi.lib.action.I2cSendConditionAction;
 import com.uxxu.konashi.lib.action.I2cSetReadParamAction;
 import com.uxxu.konashi.lib.action.I2cWriteAction;
@@ -31,18 +30,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.mockito.internal.util.reflection.Whitebox;
-import org.mockito.verification.VerificationMode;
 
 import java.util.UUID;
 
 import info.izumin.android.bletia.Bletia;
 import info.izumin.android.bletia.BletiaException;
 import info.izumin.android.bletia.action.Action;
-import info.izumin.android.bletia.action.ReadRemoteRssiAction;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
@@ -58,22 +56,24 @@ public class KonashiManagerTest extends AndroidTestCase {
     public static final String TAG = KonashiManagerTest.class.getSimpleName();
 
     @Mock private BluetoothGattService mService;
+    @Mock private BluetoothGattCharacteristic mCharacteristic;
     @Mock private Bletia mBletia;
     @Spy private KonashiManager mKonashiManager;
 
-    private Deferred<BluetoothGattCharacteristic, BletiaException, Object> mDeferred;
-    private Promise<BluetoothGattCharacteristic, BletiaException, Object> mPromise;
+    private Deferred<BluetoothGattCharacteristic, BletiaException, Void> mDeferred;
+    private Promise<BluetoothGattCharacteristic, BletiaException, Void> mPromise;
 
-    private ArgumentCaptor<Action> mActionCaptor;
+    @Captor private ArgumentCaptor<Action<BluetoothGattCharacteristic, UUID>> mActionCaptor;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         mKonashiManager = new KonashiManager();
-        mActionCaptor = ArgumentCaptor.forClass(Action.class);
         mDeferred = new DeferredObject<>();
         mPromise = mDeferred.promise();
         when(mBletia.getService(any(UUID.class))).thenReturn(mService);
+        when(mService.getCharacteristic(any(UUID.class))).thenReturn(mCharacteristic);
+        when(mCharacteristic.getUuid()).thenReturn(UUID.randomUUID());
         when(mBletia.execute(mActionCaptor.capture())).thenReturn(mPromise);
         Whitebox.setInternalState(mKonashiManager, "mBletia", mBletia);
     }
