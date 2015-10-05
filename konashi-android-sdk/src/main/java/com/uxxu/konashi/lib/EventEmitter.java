08cfaee126e24cb31bf9c4f64e6676b6fbe35337
izumin5210 <masayuki@izumin.info>
2015-10-05 23:36:03 +0900

    Run callbacks of actions on UI thread

--- konashi-android-sdk/src/main/java/com/uxxu/konashi/lib/EventEmitter.java
+++ konashi-android-sdk/src/main/java/com/uxxu/konashi/lib/EventEmitter.java
@@ -1,5 +1,8 @@
 package com.uxxu.konashi.lib;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import java.util.ArrayList;
 
 import info.izumin.android.bletia.BletiaException;
@@ -8,39 +11,73 @@ import info.izumin.android.bletia.BletiaException;
  * Created by izumin on 9/23/15.
  */
 class EventEmitter extends ArrayList<KonashiListener> {
-    public void emitConnect(KonashiManager manager) {
-        for (KonashiListener listener : this) {
-            listener.onConnect(manager);
-        }
+
+    private final EventEmitter self = this;
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
+
+    public void emitConnect(final KonashiManager manager) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                for (KonashiListener listener : self) {
+                    listener.onConnect(manager);
+                }
+            }
+        });
     }
 
-    public void emitDisconnect(KonashiManager manager) {
-        for (KonashiListener listener : this) {
-            listener.onDisconnect(manager);
-        }
+    public void emitDisconnect(final KonashiManager manager) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                for (KonashiListener listener : self) {
+                    listener.onDisconnect(manager);
+                }
+            }
+        });
     }
 
-    public void emitError(KonashiManager manager, BletiaException e) {
-        for (KonashiListener listener : this) {
-            listener.onError(manager, e);
-        }
+    public void emitError(final KonashiManager manager, final BletiaException e) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                for (KonashiListener listener : self) {
+                    listener.onError(manager, e);
+                }
+            }
+        });
     }
 
-    public void emitUpdatePioOutput(KonashiManager manager, int value) {
-        for (KonashiListener listener : this) {
-            listener.onUpdatePioOutput(manager, value);
-        }
+    public void emitUpdatePioOutput(final KonashiManager manager, final int value) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                for (KonashiListener listener : self) {
+                    listener.onUpdatePioOutput(manager, value);
+                }
+            }
+        });
     }
 
-    public void emitUpdateUartRx(KonashiManager manager, byte[] value) {
-        for (KonashiListener listener : this) {
-            listener.onUpdateUartRx(manager, value);
-        }
+    public void emitUpdateUartRx(final KonashiManager manager, final byte[] value) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                for (KonashiListener listener : self) {
+                    listener.onUpdateUartRx(manager, value);
+                }
+            }
+        });
     }
 
-    public void emitUpdateBatteryLevel(KonashiManager manager, int level) {
-        for (KonashiListener listener : this) {
-            listener.onUpdateBatteryLevel(manager, level);
-        }
+    public void emitUpdateBatteryLevel(final KonashiManager manager, final int level) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                for (KonashiListener listener : self) {
+                    listener.onUpdateBatteryLevel(manager, level);
+                }
+            }
+        });
     }
 }
