3c8909e3ac9ed1363276a074ec58b22c59995647
e10dokup <1365196t@stu.kobe-u.ac.jp>
2015-10-07 10:12:20 +0900

    Add components to app

---
+++ samples/pwm-sample/src/main/java/com/uxxu/konashi/sample/pwmsample/MainActivity.java
@@ -0,0 +1,7 @@
+package com.uxxu.konashi.sample.pwmsample;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+}
